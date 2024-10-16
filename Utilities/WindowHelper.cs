ec337143663917f7b72a58358d8dd335fdf50978
Adam Schoenfeld <adamrschoenfeld311@gmail.com>
2024-10-16 02:10:23 -0700

    Added new SetForegroundHelper class
    This class contains helper functions to find a window by its title and then set that window as the foreground window

--- Utilities/WindowHelper.cs
+++ Utilities/WindowHelper.cs
@@ -1,4 +1,10 @@
 ﻿using Playnite.SDK;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
 using System.Windows;
 using System.Windows.Forms;
 using System.Windows.Interop;
@@ -6,8 +12,72 @@ using System.Windows.Media;
 
 namespace LaunchMate.Utilities
 {
+
+    /// <summary>
+    /// Stack overflow magic from <see href="https://stackoverflow.com/a/2584672"/> and <see href="https://stackoverflow.com/a/49822416"/>
+    /// </summary>
+    public class SetForegroundHelper
+    {
+        public static void SetForeground(string windowTitle)
+        {
+            const uint WM_GETTEXT = 0x000D;
+            foreach (var handle in SetForegroundHelper.EnumerateProcessWindowHandles(Process.GetProcessesByName("Playnite.DesktopApp").First().Id))
+            {
+                StringBuilder message = new StringBuilder(1000);
+                SetForegroundHelper.SendMessage(handle, WM_GETTEXT, message.Capacity, message);
+                if (message.ToString().Contains(windowTitle))
+                {
+                    SetForegroundHelper.SetForegroundWindow(handle);
+                }
+            }
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, int wParam, StringBuilder lParam);
+
+        [System.Runtime.InteropServices.DllImport("User32.dll")]
+        public static extern bool SetForegroundWindow(IntPtr handle);
+
+        public delegate bool EnumThreadDelegate(IntPtr hWnd, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        public static extern bool EnumThreadWindows(int dwThreadId, EnumThreadDelegate lpfn,
+            IntPtr lParam);
+
+        public static IEnumerable<IntPtr> EnumerateProcessWindowHandles(int processId)
+        {
+            var handles = new List<IntPtr>();
+
+            foreach (ProcessThread thread in Process.GetProcessById(processId).Threads)
+                EnumThreadWindows(thread.Id,
+                    (hWnd, lParam) => { handles.Add(hWnd); return true; }, IntPtr.Zero);
+
+            return handles;
+        }
+    }
+
     public static class WindowHelper
     {
+
+        /// <summary>
+        /// Get a width and a height near the max size of the user's screen
+        /// </summary>
+        /// <param name="title"></param>
+        /// <returns>A Tuple<int, int> where item1 is the width and item2 is the height</returns>
+        public static Tuple<int, int> GetNearMaxWindow(string title)
+        {
+            var window = CreateWindow(title);
+            var ioHelper = new WindowInteropHelper(window.Owner);
+            var hWnd = ioHelper.Handle;
+            var screen = Screen.FromHandle(hWnd);
+            var dpi = VisualTreeHelper.GetDpi(window);
+            int width = (int)(screen.WorkingArea.Width * 0.96D / dpi.DpiScaleY);
+            int height = (int)(screen.WorkingArea.Height * 0.96D / dpi.DpiScaleY);
+            window.Close();
+            return Tuple.Create(width, height);
+
+        }
+
         /// <summary>
         /// Creates a <see cref="Window"/> with the given title, width, and height
         /// </summary>
@@ -58,6 +128,8 @@ namespace LaunchMate.Utilities
 
             return window;
         }
+
+
 
         private static Window CreateWindow(string title, bool showMaximizeButton = false, bool showMinimizeButton = false)
         {
